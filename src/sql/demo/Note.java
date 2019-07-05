55d4106f954cc487095a9a56a39c08ad717b6f4e
mashapoli <mashapolichuk911@gmail.com>
2019-07-06 02:43:02 +0300

    in progress

--- src/sql/demo/Note.java
+++ src/sql/demo/Note.java
@@ -1,47 +1,38 @@
 package sql.demo;
 
-import java.sql.*;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Note {
 
-    private int supId;
+    private Integer id;
     private static AtomicInteger count = new AtomicInteger(0);
     private String title;
     private String content;
     private Date createdOn;
     private Date updateOn;
 
-    public static final String SUP_ID = "sup_id";
+    public static final String ID = "id";
     public static final String TITLE = "title";
     public static final String CONTENT = "content";
     public static final String CREATED_ON = "created_on";
     public static final String UPDATE_ON = "update_on";
 
     public Note() {
-        supId = count.incrementAndGet();
         createdOn = new Date();
         updateOn = new Date();
     }
-
+    public Note(Map<String, Object> row){
+        id = (Integer) row.get(ID);
+        title = (String) row.get(TITLE);
+        content = (String) row.get(CONTENT);
+        createdOn = (Date) row.get(CREATED_ON);
+        updateOn = (Date) row.get(UPDATE_ON);
+    }
     @Override
     public String toString() {
         return getTitle();
-    }
-
-    public Map<String, Object> toNotes(ResultSet rs) throws SQLException {
-        ResultSetMetaData md = rs.getMetaData();
-        int colums = md.getColumnCount();
-        Map<String, Object> row = new HashMap<String, Object>(colums);
-        row.put(SUP_ID, supId);
-        row.put(TITLE, title);
-        row.put(CONTENT, content);
-        row.put(CREATED_ON, createdOn);
-        row.put(UPDATE_ON, updateOn);
-        return row;
     }
 
     public Date getCreatedOn() {
@@ -60,8 +51,8 @@ public class Note {
         updateOn = date;
     }
 
-    public int getSupId() {
-        return supId;
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
