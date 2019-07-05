2e767c9ee614d7fe752eae4b0c897427b0c85265
mashapoli <mashapolichuk911@gmail.com>
2019-07-06 00:41:49 +0300

    in progress

--- src/sql/demo/Main.java
+++ src/sql/demo/Main.java
@@ -1,6 +1,4 @@
 package sql.demo;
-import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
-
 import java.sql.*;
 import java.util.*;
 
@@ -8,11 +6,9 @@ import java.util.*;
 public class Main {
     public static List<Map<String, Object>> executeQuery(Connection con, String query)
             throws SQLException {
-        Statement stmt = null;
         query = "SUP_ID, TITLE, CONTENT, CREATED_ON, UPDATE_ON ";
         List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
-        try {
-            stmt = con.createStatement();
+        try (Statement stmt = con.createStatement()){
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData  md = rs.getMetaData();
             int colums = md.getColumnCount();
@@ -23,20 +19,8 @@ public class Main {
                 }
                 mapList.add(row);
             }
-            while (rs.next()) {
-                int supplierID = rs.getInt("SUP_ID");
-                String title = rs.getString("TITLE");
-                String content = rs.getString("CONTENT");
-                Timestamp created_on = rs.getTimestamp("CREATED_ON");
-                Timestamp update_on = rs.getTimestamp("UPDATE_ON");
-                System.out.println(supplierID + "\t" + title +
-                        "\t" + content + "\t" + created_on +
-                        "\t" + update_on);
-            }
         } catch (SQLException e ) {
-            e.printStackTrace(); //JDBCTutorialUtilities.printSQLException(e);
-        } finally {
-            if (stmt != null) { stmt.close(); }
+            e.printStackTrace();
         }
         return mapList;
     }
