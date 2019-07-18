1330fb03fff5ade921193f162029753bcb7efc1b
mashapoli <mashapolichuk911@gmail.com>
2019-07-17 22:17:37 -0500

    in progress

--- src/sql/demo/NotesFrame.java
+++ src/sql/demo/NotesFrame.java
@@ -8,150 +8,75 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NotesFrame extends JFrame{
-    private JTextField textField;
-    private JButton createNote;
-    private JButton showAllNotes;
-    private JButton showNotesPeriod;
-    private  ActionListener actionListener;
 
     public NotesFrame(){
+
         super("Notes");
-
-        createGUI();
-        CreateNote();
-        ShowAllNotes();
-        ShowNotesPeriod();
-    }
-
-    public void createGUI(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
 
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("File");
-        mb.add(m1);
-        JMenuItem m11 = new JMenuItem("Create new Note");
-        JMenuItem m22 = new JMenuItem("Save");
-        JMenuItem m33 = new JMenuItem("Remove note");
-        JMenuItem m44 = new JMenuItem("Show all notes");
-        m1.add(m11);
-        m1.add(m22);
-        m1.add(m33);
-        m1.add(m44);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout(5, 5));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        JPanel periodPanel = new JPanel(new FlowLayout());
+        periodPanel.setBorder(BorderFactory.createTitledBorder("Period"));
 
-        createNote = new JButton("Create Note");
-        panel.add(createNote);
+        JLabel from = new JLabel("From");
+        from.setLayout(new BorderLayout());
+        String sFrom = "ex.2019-06-30 20:18:54.7";
+        JTextField fieldF = new JTextField();
+        fieldF.setText(sFrom);
 
-        showAllNotes = new JButton("Show all Notes");
-        panel.add(showAllNotes);
+        periodPanel.add(from);
+        periodPanel.add(fieldF);
 
-        showNotesPeriod = new JButton("Show Notes for the period");
-        panel.add(showNotesPeriod);
+        JLabel to = new JLabel("To");
+        from.setLayout(new BorderLayout());
+        String sTo = "ex.2019-07-30 20:18:54.7";
+        JTextField fieldTo = new JTextField();
+        fieldTo.setText(sTo);
 
-        actionListener = new NotesFrame.TestActionListener();
+        periodPanel.add(to);
+        periodPanel.add(fieldTo);
 
-        getContentPane().add(BorderLayout.CENTER, panel);
-        getContentPane().add(BorderLayout.NORTH, mb);
+        JPanel notes = new JPanel(new FlowLayout());
+        notes.setBorder(BorderFactory.createTitledBorder("Notes"));
 
-}
-    public void CreateNote() {
+        JButton newNotes = new JButton("New Notes");
+        JButton edit = new JButton("Edit");
+        JButton delete = new JButton("Delete");
 
-        createNote.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFrame form2 = new JFrame("Create Note");
-                form2.setSize(600, 600);
+        JPanel button = new JPanel(new FlowLayout());
+        button.setBorder(BorderFactory.createTitledBorder(""));
 
-                JMenuBar mb = new JMenuBar();
-                JMenu m1 = new JMenu("File");
-                mb.add(m1);
-                JMenuItem m11 = new JMenuItem("Create new Note");
-                JMenuItem m22 = new JMenuItem("Save");
-                JMenuItem m33 = new JMenuItem("Remove note");
-                JMenuItem m44 = new JMenuItem("Show all notes");
-                m1.add(m11);
-                m1.add(m22);
-                m1.add(m33);
-                m1.add(m44);
+        button.add(newNotes);
+        button.add(edit);
+        button.add(delete);
 
-                form2.getContentPane().add(BorderLayout.NORTH, mb);
-
-                JPanel panel = new JPanel();
-                panel.setLayout(new FlowLayout());
-
-                JPanel panelTitle = new JPanel();
-                panelTitle.setLayout(new FlowLayout());
-
-                JPanel panelContent = new JPanel();
-                panelContent.setLayout(new FlowLayout());
-
-                JButton save = new JButton("Save");
-                panel.add(save);
-
-                JButton delete = new JButton("Delete");
-                panel.add(delete);
-
-                JLabel title = new JLabel("Title");
-                JTextField tf = new JTextField(20);
-                panelTitle.add(title);
-                panelTitle.add(tf);
-
-                JLabel content = new JLabel("Content");
-                JTextArea ta = new JTextArea();
-                panelContent.add(content);
-
-                form2.getContentPane().add(BorderLayout.WEST,panelTitle);
-                //form2.getContentPane().add(BorderLayout.BEFORE_FIRST_LINE,panelContent);
-                form2.getContentPane().add(BorderLayout.CENTER,ta);
-                form2. getContentPane().add(BorderLayout.SOUTH, panel);
+        mainPanel.add(periodPanel, BorderLayout.NORTH);
+        mainPanel.add(notes, BorderLayout.CENTER);
+        mainPanel.add(button, BorderLayout.SOUTH);
 
 
+        getContentPane().add(mainPanel);
 
-                NotesFrame frame = new NotesFrame();
-                frame.setVisible(false);
-                form2.setVisible(true);
-            }
-        });
+        JButton show = new JButton("Show");
+        periodPanel.add(show);
+
+
+        setPreferredSize(new Dimension(600, 600));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
-
-   public void ShowAllNotes(){
-       showAllNotes.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               JFrame form2 = new JFrame("Show all Notes");
-               form2.setSize(600, 600);
-               NotesFrame frame = new NotesFrame();
-               frame.setVisible(false);
-               form2.setVisible(true);
-           }
-       });
-   }
-
-    public void ShowNotesPeriod(){
-        showNotesPeriod.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFrame form2 = new JFrame(" Show Notes for the period");
-                form2.setSize(600, 600);
-                NotesFrame frame = new NotesFrame();
-                frame.setVisible(false);
-                form2.setVisible(true);
-            }
-        });
-    }
-
-    public class TestActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            textField.setText(e.getActionCommand());
-        }
-    }
-
-
 
     public static void main(String args[]){
-                NotesFrame frame = new NotesFrame();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                new NotesFrame();
+            }
+        });
     }
 
 }
